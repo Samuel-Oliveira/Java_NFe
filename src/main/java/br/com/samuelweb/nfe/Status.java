ddb4fd95e331666725702bcec08ca4f2c104fa9e
Cristofer <cristoferbonmann@gmail.com'>
2019-01-13 21:28:37 -0200

    Complementação ao JavaDoc do método statusServico da classe Status.

--- src/main/java/br/com/samuelweb/nfe/Status.java
+++ src/main/java/br/com/samuelweb/nfe/Status.java
@@ -18,7 +18,7 @@ import br.inf.portalfiscal.nfe.schema_4.retConsStatServ.TRetConsStatServ;
 import br.inf.portalfiscal.www.nfe_400.wsdl.NFeStatusServico4.NFeStatusServico4Stub;
 
 /**
- * Classe responsável por fazer a Verificacao do Status Do Webservice
+ * Classe responsável por fazer a Verificação do Status Do Webservice
  * 
  * @author Samuel Oliveira
  *
@@ -27,8 +27,22 @@ class Status {
 
 	/**
 	 * Metodo para Consulta de Status de Serviço
-	 *
-         * @param confi ConfiguracoesNfe
+         *
+	 * Cria um objeto do tipo TConsStatServ usando as propriedades passadas
+         *  pelo parâmetro config. Após, este objeto é convertido em um obejto
+         * OMElement manipulável onde é passado para o atributo extraElement da
+         * classe NFeStatusServico4Stub.NfeDadosMsg.
+         * 
+         * O método statusServico então cria uma instância de NFeStatusServico4Stub
+         * passando o parâmetro tipo e config em seu construtor, onde será montada a URL
+         * de consulta do status do serviço dependendo das configuções
+         * (ambiente, Estado, NF-e ou NFC-e)
+         * 
+         * Então o método nfeStatusServicoNF efetuará a consulta e retorna o 
+         * resultado que será convertido em um objeto e enfim retornado por este
+         * método.
+         * 
+         * @param confi ConfiguracoesNfe, interface de configuração da NF-e ou NFC-e.
 	 * @param tipo ConstantesUtil.NFE ou ConstantesUtil.NFCE
 	 * @return TRetConsStatServ - objeto que contém o resultado
          * da transmissão do XML.
@@ -36,36 +50,38 @@ class Status {
          * 
          * @see ConfiguracoesNfe
          * @see ConstantesUtil
+         * @see WebServiceUtil
+         * @see XmlUtil
 	 */
 	static TRetConsStatServ statusServico(ConfiguracoesNfe config, String tipo) throws NfeException {
 
-		try {
+            try {
 
-			TConsStatServ consStatServ = new TConsStatServ();
-			consStatServ.setTpAmb(config.getAmbiente());
-			consStatServ.setCUF(config.getEstado().getCodigoIbge());
-			consStatServ.setVersao(config.getVersaoNfe());
-			consStatServ.setXServ("STATUS");
-			String xml = XmlUtil.objectToXml(consStatServ);
+                TConsStatServ consStatServ = new TConsStatServ();
+                consStatServ.setTpAmb(config.getAmbiente());
+                consStatServ.setCUF(config.getEstado().getCodigoIbge());
+                consStatServ.setVersao(config.getVersaoNfe());
+                consStatServ.setXServ("STATUS");
+                String xml = XmlUtil.objectToXml(consStatServ);
 
-			if (config.isLog()) {
-				System.out.println("Xml Status: " + xml);
-			}
-			OMElement ome = AXIOMUtil.stringToOM(xml);
+                if (config.isLog()) {
+                    System.out.println("Xml Status: " + xml);
+                }
+                OMElement ome = AXIOMUtil.stringToOM(xml);
 
-			NFeStatusServico4Stub.NfeDadosMsg dadosMsg = new NFeStatusServico4Stub.NfeDadosMsg();
-			dadosMsg.setExtraElement(ome);
+                NFeStatusServico4Stub.NfeDadosMsg dadosMsg = new NFeStatusServico4Stub.NfeDadosMsg();
+                dadosMsg.setExtraElement(ome);
 
-			NFeStatusServico4Stub stub = new NFeStatusServico4Stub(tipo.equals(ConstantesUtil.NFCE)
-					? WebServiceUtil.getUrl(config, ConstantesUtil.NFCE, ConstantesUtil.SERVICOS.STATUS_SERVICO)
-					: WebServiceUtil.getUrl(config, ConstantesUtil.NFE, ConstantesUtil.SERVICOS.STATUS_SERVICO));
-			NFeStatusServico4Stub.NfeResultMsg result = stub.nfeStatusServicoNF(dadosMsg);
+                NFeStatusServico4Stub stub = new NFeStatusServico4Stub(tipo.equals(ConstantesUtil.NFCE)
+                        ? WebServiceUtil.getUrl(config, ConstantesUtil.NFCE, ConstantesUtil.SERVICOS.STATUS_SERVICO)
+                        : WebServiceUtil.getUrl(config, ConstantesUtil.NFE, ConstantesUtil.SERVICOS.STATUS_SERVICO));
+                NFeStatusServico4Stub.NfeResultMsg result = stub.nfeStatusServicoNF(dadosMsg);
 
-			return XmlUtil.xmlToObject(result.getExtraElement().toString(), TRetConsStatServ.class);
+                return XmlUtil.xmlToObject(result.getExtraElement().toString(), TRetConsStatServ.class);
 
-		} catch (RemoteException | XMLStreamException | JAXBException e) {
-			throw new NfeException(e.getMessage());
-		}
+            } catch (RemoteException | XMLStreamException | JAXBException e) {
+                throw new NfeException(e.getMessage());
+            }
 	}
 
 }
