a7715fbad1e324b850db6cdca754d285eae14bba
Cristofer <cristoferbonmann@gmail.com'>
2019-01-15 00:19:27 -0200

    Adicionado JavaDoc para o método getUrlConsultaCadastro na classe WebServiceUtil.

--- src/main/java/br/com/samuelweb/nfe/util/WebServiceUtil.java
+++ src/main/java/br/com/samuelweb/nfe/util/WebServiceUtil.java
@@ -13,7 +13,7 @@ import java.io.InputStream;
 /**
  * @author Samuel Oliveira
  *
- * Classe responsávelem montar a URL de consulta de serviços do SEFAZ.
+ * Classe responsávelem montar as URL's de consulta de serviços do SEFAZ.
  */
 public class WebServiceUtil {
 
@@ -96,11 +96,36 @@ public class WebServiceUtil {
 
     }
 
-	public static String getUrlConsultaCadastro(ConfiguracoesNfe config, String uf) throws NfeException {
+     /**
+     * Método que retorna a URL para consulta de cadastro de contribuinte ICMS<p>
+     * 
+     * O método primeiramente montará a String <b>secao</b>, que representa o 
+     * grupo de onde se localiza a URL desejada no arquivo <b>WebServicesNfe.ini.</b>
+     * Após, irá carregar o arquivo .ini e buscar efetivamente a URL nessa seção
+     * e no caso deste método, buscará pelo valor da chave
+     * ConstantesUtil.SERVICOS.CONSULTA_CADASTRO. Então retornará esse valor,
+     * que é a URL de consulta.<p>
+     * 
+     * Caso a URL não seja encontrada ou se ocorrer algum problema na leitura
+     * do arquivo .ini, será disparado um NfeException.
+     * 
+     * Esse método é chamado por outro método chamado <b>consultaCadastro</b> 
+     * na classe <b>ConsultaCadastro</b>
+     * 
+     * @param config interface que contêm os dados necessários para a comunicação.
+     * @param uf Estado do CNPJ que será consultado.
+     * @return url String que representa a URL a ser consultada.
+     * @throws NfeException
+     * 
+     * @see ConstantesUtil
+     * @see br.com.samuelweb.nfe.Nfe
+     * @see br.com.samuelweb.nfe.ConsultaCadastro#consultaCadastro
+     */
+    public static String getUrlConsultaCadastro(ConfiguracoesNfe config, String uf) throws NfeException {
 
-		String tipo = ConstantesUtil.NFE;
-		String servico = ConstantesUtil.SERVICOS.CONSULTA_CADASTRO;
-		try {
+        String tipo = ConstantesUtil.NFE;
+        String servico = ConstantesUtil.SERVICOS.CONSULTA_CADASTRO;
+        try {
 
             String secao;
             if (uf.toUpperCase().equals(Estados.AC.toString()) || uf.toUpperCase().equals(Estados.RN.toString())
@@ -113,26 +138,26 @@ public class WebServiceUtil {
                         + (config.getAmbiente().equals(ConstantesUtil.AMBIENTE.HOMOLOGACAO) ? "H" : "P");
             }
 
-			InputStream is = WebServiceUtil.class.getResourceAsStream("/WebServicesNfe.ini");
+            InputStream is = WebServiceUtil.class.getResourceAsStream("/WebServicesNfe.ini");
 
-			Wini ini = new Wini();
-			ini.getConfig().setLowerCaseOption(true);
-			ini.load(is);
-			String url = ini.get(secao, servico.toLowerCase());
+            Wini ini = new Wini();
+            ini.getConfig().setLowerCaseOption(true);
+            ini.load(is);
+            String url = ini.get(secao, servico.toLowerCase());
 
-			if (ObjetoUtil.isEmpty(url)) {
-				throw new NfeException("WebService de " + servico + " não encontrado para " + uf);
-			}
+            if (ObjetoUtil.isEmpty(url)) {
+                throw new NfeException("WebService de " + servico + " não encontrado para " + uf);
+            }
 
-			if (config.isLog()) {
-				System.out.println("WebService - " + url);
-			}
-			return url;
+            if (config.isLog()) {
+                System.out.println("WebService - " + url);
+            }
+            return url;
 
-		} catch (IOException e) {
-			throw new NfeException(e.getMessage());
-		}
+        } catch (IOException e) {
+            throw new NfeException(e.getMessage());
+        }
 
-	}
+    }
 
 }
