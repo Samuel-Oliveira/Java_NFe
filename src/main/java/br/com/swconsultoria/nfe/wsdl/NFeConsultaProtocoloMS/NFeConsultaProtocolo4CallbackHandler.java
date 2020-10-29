1634a54c18ee6391b24f72c2092746fcd0b8fab3
arimolo <arimolo@gmail.com>
2020-10-29 14:19:03 -0300

    [Issue #196] Corrigindo o erro 'Rejeicao: 587 (Usar somente o namespace padrao da NF-e)' retornado pela SEFAZ do MS. Gerei novo stub específico para uso em chamadas do MS.

---
+++ src/main/java/br/com/swconsultoria/nfe/wsdl/NFeConsultaProtocoloMS/NFeConsultaProtocolo4CallbackHandler.java
@@ -0,0 +1,55 @@
+/**
+ * NFeConsultaProtocolo4CallbackHandler.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis2 version: 1.7.5  Built on : May 06, 2017 (03:45:26 BST)
+ */
+package br.com.swconsultoria.nfe.wsdl.NFeConsultaProtocoloMS;
+
+
+/**
+ *  NFeConsultaProtocolo4CallbackHandler Callback class, Users can extend this class and implement
+ *  their own receiveResult and receiveError methods.
+ */
+public abstract class NFeConsultaProtocolo4CallbackHandler {
+    protected Object clientData;
+
+    /**
+     * User can pass in any object that needs to be accessed once the NonBlocking
+     * Web service call is finished and appropriate method of this CallBack is called.
+     * @param clientData Object mechanism by which the user can pass in user data
+     * that will be avilable at the time this callback is called.
+     */
+    public NFeConsultaProtocolo4CallbackHandler(Object clientData) {
+        this.clientData = clientData;
+    }
+
+    /**
+     * Please use this constructor if you don't want to set any clientData
+     */
+    public NFeConsultaProtocolo4CallbackHandler() {
+        this.clientData = null;
+    }
+
+    /**
+     * Get the client data
+     */
+    public Object getClientData() {
+        return clientData;
+    }
+
+    /**
+     * auto generated Axis2 call back method for nfeConsultaNF method
+     * override this method for handling normal response from nfeConsultaNF operation
+     */
+    public void receiveResultnfeConsultaNF(
+        NFeConsultaProtocolo4Stub.NfeResultMsg result) {
+    }
+
+    /**
+     * auto generated Axis2 Error handler
+     * override this method for handling error response from nfeConsultaNF operation
+     */
+    public void receiveErrornfeConsultaNF(Exception e) {
+    }
+}
