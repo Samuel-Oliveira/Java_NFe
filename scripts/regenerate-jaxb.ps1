8e32078178b393bc2aa712f18ee2e7e666e898ee
SamuelOliveira <samuk.exe@hotmail.com>
2026-08-18 20:02:08 -0300

    Corrigido Schemas

--- scripts/regenerate-jaxb.ps1
+++ scripts/regenerate-jaxb.ps1
@@ -239,7 +239,6 @@ Invoke-XjcPass `
     -Label "schemas_eventos pass-01: generico" `
     -Package "$BasePkg.schemas_eventos" `
     -XsdFiles @(
-        '_types_legado_v1.xsd'
         'leiauteEvento_v1.00.xsd'
         'envEvento_v1.00.xsd'
         'retEnvEvento_v1.00.xsd'
@@ -252,7 +251,6 @@ Invoke-XjcPass `
     -Label "schemas_eventos pass-02: cancelamento" `
     -Package "$BasePkg.schemas_eventos" `
     -XsdFiles @(
-        '_types_legado_v1.xsd'
         'leiauteEventoCancNFe_v1.00.xsd'
         'eventoCancNFe_v1.00.xsd'
         'envEventoCancNFe_v1.00.xsd'
@@ -266,7 +264,6 @@ Invoke-XjcPass `
     -Label "schemas_eventos pass-03: cancelamento-substituicao" `
     -Package "$BasePkg.schemas_eventos" `
     -XsdFiles @(
-        '_types_legado_v1.xsd'
         'leiauteEventoCancSubst_v1.00.xsd'
         'eventoCancSubst_v1.00.xsd'
         'envEventoCancSubst_v1.00.xsd'
@@ -280,7 +277,6 @@ Invoke-XjcPass `
     -Label "schemas_eventos pass-04: carta-correcao" `
     -Package "$BasePkg.schemas_eventos" `
     -XsdFiles @(
-        '_types_legado_v1.xsd'
         'leiauteCCe_v1.00.xsd'
         'CCe_v1.00.xsd'
         'envCCe_v1.00.xsd'
@@ -294,7 +290,6 @@ Invoke-XjcPass `
     -Label "schemas_eventos pass-05: epec" `
     -Package "$BasePkg.schemas_eventos" `
     -XsdFiles @(
-        '_types_legado_v1.xsd'
         'leiauteEPEC_v1.00.xsd'
         'EPEC_v1.00.xsd'
         'envEPEC_v1.00.xsd'
@@ -308,7 +303,6 @@ Invoke-XjcPass `
     -Label "schemas_eventos pass-06: manifestacao" `
     -Package "$BasePkg.schemas_eventos" `
     -XsdFiles @(
-        '_types_legado_v1.xsd'
         'leiauteConfRecebto_v1.00.xsd'
         'confRecebto_v1.00.xsd'
         'envConfRecebto_v1.00.xsd'
@@ -322,7 +316,6 @@ Invoke-XjcPass `
     -Label "schemas_eventos pass-07: conciliacao-financeira" `
     -Package "$BasePkg.schemas_eventos" `
     -XsdFiles @(
-        '_types_legado_v1.xsd'
         'leiauteEventoEConf_v1.00.xsd'
         'EventoEConf_v1.00.xsd'
         'envEventoEConf_v1.00.xsd'
@@ -336,7 +329,6 @@ Invoke-XjcPass `
     -Label "schemas_eventos pass-08: cancelamento-conciliacao-financeira" `
     -Package "$BasePkg.schemas_eventos" `
     -XsdFiles @(
-        '_types_legado_v1.xsd'
         'leiauteEventoCancEConf_v1.00.xsd'
         'EventoCancEConf_v1.00.xsd'
         'envEventoCancEConf_v1.00.xsd'
@@ -350,7 +342,6 @@ Invoke-XjcPass `
     -Label "schemas_eventos pass-09: insucesso-entrega" `
     -Package "$BasePkg.schemas_eventos" `
     -XsdFiles @(
-        '_types_legado_v1.xsd'
         'leiauteEventoInsucessoNFe_v1.00.xsd'
         'EventoInsucessoNFe_v1.00.xsd'
         'envEventoInsucessoNFe_v1.00.xsd'
@@ -366,7 +357,6 @@ Invoke-XjcPass `
     -Label "schemas_eventos pass-10: cancelamento-insucesso-entrega" `
     -Package "$BasePkg.schemas_eventos" `
     -XsdFiles @(
-        '_types_legado_v1.xsd'
         'leiauteEventoCancInsucessoNFe_v1.00.xsd'
         'EventoCancInsucessoNFe_v1.00.xsd'
         'envEventoCancInsucessoNFe_v1.00.xsd'
@@ -379,7 +369,6 @@ Invoke-XjcPass `
     -Label "schemas_eventos pass-11: comprovante-entrega" `
     -Package "$BasePkg.schemas_eventos" `
     -XsdFiles @(
-        '_types_legado_v1.xsd'
         'leiauteEventoEntregaNFe_v1.00.xsd'
         'EventoEntregaNFe_v1.00.xsd'
         'envEventoEntregaNFe_v1.00.xsd'
@@ -393,7 +382,6 @@ Invoke-XjcPass `
     -Label "schemas_eventos pass-12: cancelamento-comprovante-entrega" `
     -Package "$BasePkg.schemas_eventos" `
     -XsdFiles @(
-        '_types_legado_v1.xsd'
         'leiauteEventoCancEntregaNFe_v1.00.xsd'
         'EventoCancEntregaNFe_v1.00.xsd'
         'envEventoCancEntregaNFe_v1.00.xsd'
@@ -407,7 +395,6 @@ Invoke-XjcPass `
     -Label "schemas_eventos pass-13: ator-interessado" `
     -Package "$BasePkg.schemas_eventos" `
     -XsdFiles @(
-        '_types_legado_v1.xsd'
         'leiauteEventoAtorInteressado_v1.00.xsd'
         'eventoAtorInteressado_v1.00.xsd'
         'envEventoAtorInteressado_v1.00.xsd'
